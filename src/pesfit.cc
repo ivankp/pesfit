8bd2000cb109b78a5185e995769a7578f0609f7d
ivankp <ivan.pogrebnyak@gmail.com>
2015-10-18 23:12:47 -0500

    gaus fit

--- src/pesfit.cc
+++ src/pesfit.cc
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
+#include <initializer_list>
+#include <memory>
 
 #include <boost/program_options.hpp>
 
@@ -8,9 +11,13 @@
 #include <TTree.h>
 #include <TDirectory.h>
 #include <TH1.h>
+#include <TFitResult.h>
+#include <TFitResultPtr.h>
 #include <TCanvas.h>
+#include <TLatex.h>
 
 #include "catstr.hh"
+#include "senum.hh"
 
 using namespace std;
 namespace po = boost::program_options;
@@ -36,17 +43,87 @@ inline T* get(TDirectory* d, const char* name) {
 }
 
 // options ------------------
+senum(Fit,(none)(gaus)(cb))
+Fit::type fit;
+
 string ofname, cfname;
 vector<string> ifname;
 vector<Color_t> colors;
 Int_t nbins;
+bool logy;
 // --------------------------
 
-template<typename T>
-inline void SetColor(T* obj, int i) noexcept {
-  const Color_t color = colors[i%colors.size()];
-  obj->SetLineColor(color);
-  obj->SetMarkerColor(color);
+// global -------------------
+TTree *tree;
+TCanvas *canv;
+TLatex *lbl;
+// --------------------------
+
+void make_hist(TH1*& hist, const char* name, const char* branch, double scale) {
+  const string cmd(cat(branch,"/1000>>hist(",nbins,",105,160)"));
+  tree->Draw( cmd.c_str(),
+    "HGamEventInfoAuxDyn.weight*(HGamEventInfoAuxDyn.isPassed==1)"
+  );
+  cout << cmd << endl;
+  TH1 *temp = get<TH1>(gDirectory,"hist");
+  temp->Scale(scale);
+  if (!hist)
+    (hist = (TH1*)temp->Clone(name))->SetDirectory(0);
+  else hist->Add(temp);
+}
+
+void draw(const initializer_list<TH1*>& hs) {
+  int i=0;
+  Color_t color;
+  vector<unique_ptr<TLatex>> lblp;
+  lblp.reserve(15);
+  auto latex = [&lblp](TLatex* lbl, Double_t x, Double_t y, const char* text) {
+    auto *p = lbl->DrawLatex(x,y,text);
+    lblp.emplace_back(p);
+    return p;
+  };
+  for (auto it=hs.begin(), end=hs.end(); it!=end; ++it) {
+    TH1 *hist = *it;
+    hist->SetStats(false);
+    hist->SetLineWidth(2);
+    color = colors[(i++) % colors.size()];
+    hist->SetLineColor(color);
+    hist->SetMarkerColor(color);
+
+    if (it==hs.begin()) {
+      hist->SetXTitle("m_{#gamma#gamma} [GeV]");
+      hist->Draw();
+      latex(lbl,.67,0.88-0.04*i,"Entries");
+      latex(lbl,.77,0.88-0.04*i,"mean");
+      latex(lbl,.87,0.88-0.04*i,"stdev");
+    } else hist->Draw("same");
+
+    Double_t mean, stdev;
+
+    switch (fit) {
+      case Fit::none: {
+        mean  = hist->GetMean();
+        stdev = hist->GetStdDev();
+        break; }
+      case Fit::gaus: {
+        auto fr = hist->Fit("gaus","S");
+        mean  = fr->Value(1);
+        stdev = fr->Value(2);
+        break; }
+      case Fit::cb: {
+        cerr << "cb fit not implemented yet" << endl;
+        break; }
+    }
+
+    auto lblp = latex(lbl,.52,0.84-0.04*i,hist->GetName());
+    lblp->SetTextColor(color);
+    latex(lblp,.67,0.84-0.04*i,cat(hist->GetEntries()).c_str());
+    latex(lblp,.77,0.84-0.04*i,
+      cat(fixed,setprecision(2),mean).c_str());
+    latex(lblp,.87,0.84-0.04*i,
+      cat(fixed,setprecision(2),stdev).c_str());
+  }
+  canv->SaveAs(ofname.c_str());
 }
 
 int main(int argc, char** argv)
@@ -62,6 +139,10 @@ int main(int argc, char** argv)
       ("config,c", po::value(&cfname),
        "configuration file name")
 
+      ("logy,l", po::bool_switch(&logy),
+       "logarithmic Y axis")
+      ("fit,f", po::value(&fit)->default_value(Fit::none),
+       cat("fit type: ",Fit::_str_all()).c_str())
       ("nbins,n", po::value(&nbins)->default_value(100),
        "histograms\' number of bins")
       ("colors", po::value(&colors)->multitoken()->
@@ -89,130 +170,61 @@ int main(int argc, char** argv)
     return 1;
   }
   // end options ---------------------------------------------------
-  
-  TH1 *hist, *nom, *scale_down, *scale_up, *res_down, *res_up;
 
+  TH1 *nom=nullptr,
+      *scale_down=nullptr, *scale_up=nullptr,
+      *res_down=nullptr,   *res_up=nullptr;
+
+  // LOOP over input files
   for (const string& f : ifname) {
-    static bool first = true;
-
     TFile *file = new TFile(f.c_str(),"read");
     if (file->IsZombie()) return 1;
     cout << f << endl;
-    TTree *tree = get<TTree>(file,"CollectionTree");
-    
-    const double xsecscale =
-      1./get<TH1>(file,"CutFlow_PowhegPy8_ggH125_small_weighted")->GetBinContent(3);
+    tree = get<TTree>(file,"CollectionTree");
+
+    const size_t slash = f.find('/')+1;
+    const double xsecscale = 1./get<TH1>(file,
+      ("CutFlow_"+f.substr(slash,f.find('.')-slash)+"_weighted").c_str()
+    )->GetBinContent(3);
     // regex to find the right hist
     // convert to differential cross section
     // fitt gaussian and crystal ball
-    
+
     // Is HGamEventInfoAuxDyn.crossSectionBRfilterEff name?
-    
-    // --------------------------------------------------------------
-    
-    tree->Draw(
-      cat("HGamEventInfoAuxDyn.m_yy/1000>>hist(",nbins,",105,160)").c_str(),
-      "HGamEventInfoAuxDyn.weight*(HGamEventInfoAuxDyn.isPassed==1)"
-    );
-    hist = get<TH1>(gDirectory,"hist");
-    hist->Scale(xsecscale);
-    if (first)
-      (nom = (TH1*)hist->Clone("nom"))->SetDirectory(0);
-    else nom->Add(hist);
-    
-    // --------------------------------------------------------------
-    
-    tree->Draw(
-      cat("HGamEventInfo_EG_SCALE_ALL__1downAuxDyn.m_yy/1000>>hist(",nbins,",105,160)").c_str(),
-      "HGamEventInfoAuxDyn.weight*(HGamEventInfoAuxDyn.isPassed==1)"
-    );
-    hist = get<TH1>(gDirectory,"hist");
-    hist->Scale(xsecscale);
-    if (first)
-      (scale_down = (TH1*)hist->Clone("scale_down"))->SetDirectory(0);
-    else scale_down->Add(hist);
-    
-    tree->Draw(
-      cat("HGamEventInfo_EG_SCALE_ALL__1upAuxDyn.m_yy/1000>>hist(",nbins,",105,160)").c_str(),
-      "HGamEventInfoAuxDyn.weight*(HGamEventInfoAuxDyn.isPassed==1)"
-    );
-    hist = get<TH1>(gDirectory,"hist");
-    hist->Scale(xsecscale);
-    if (first)
-      (scale_up = (TH1*)hist->Clone("scale_up"))->SetDirectory(0);
-    else scale_up->Add(hist);
-    
-    // --------------------------------------------------------------
-    
-    tree->Draw(
-      cat("HGamEventInfo_EG_RESOLUTION_ALL__1downAuxDyn.m_yy/1000>>hist(",nbins,",105,160)").c_str(),
-      "HGamEventInfoAuxDyn.weight*(HGamEventInfoAuxDyn.isPassed==1)"
-    );
-    hist = get<TH1>(gDirectory,"hist");
-    hist->Scale(xsecscale);
-    if (first)
-      (res_down = (TH1*)hist->Clone("res_down"))->SetDirectory(0);
-    else res_down->Add(hist);
-    
-    tree->Draw(
-      cat("HGamEventInfo_EG_RESOLUTION_ALL__1upAuxDyn.m_yy/1000>>hist(",nbins,",105,160)").c_str(),
-      "HGamEventInfoAuxDyn.weight*(HGamEventInfoAuxDyn.isPassed==1)"
-    );
-    hist = get<TH1>(gDirectory,"hist");
-    hist->Scale(xsecscale);
-    if (first)
-      (res_up = (TH1*)hist->Clone("res_up"))->SetDirectory(0);
-    else res_up->Add(hist);
-    
-    // --------------------------------------------------------------
-    
+
+    make_hist(nom,"nominal",
+              "HGamEventInfoAuxDyn.m_yy",xsecscale);
+    make_hist(scale_down,"scale_down",
+              "HGamEventInfo_EG_SCALE_ALL__1downAuxDyn.m_yy",xsecscale);
+    make_hist(scale_up,"scale_up",
+              "HGamEventInfo_EG_SCALE_ALL__1upAuxDyn.m_yy",xsecscale);
+    make_hist(res_down,"res_down",
+              "HGamEventInfo_EG_RESOLUTION_ALL__1downAuxDyn.m_yy",xsecscale);
+    make_hist(res_up,"res_up",
+              "HGamEventInfo_EG_RESOLUTION_ALL__1upAuxDyn.m_yy",xsecscale);
+
     delete file;
-    first = false;
   }
 
   // ---------------------------------------
 
-  TCanvas canv;
-  canv.SetMargin(0.1,0.04,0.1,0.1);
-  canv.SaveAs((ofname+'[').c_str());
+  canv = new TCanvas();
+  canv->SetMargin(0.1,0.04,0.1,0.1);
+  if (logy) canv->SetLogy();
+  canv->SaveAs((ofname+'[').c_str());
 
-  // ---------------------------------------
-    
-  nom->SetStats(false);
-  nom->SetLineWidth(2);
-  SetColor(nom,0);
-  nom->Draw();
-  canv.SaveAs(ofname.c_str());
+  lbl = new TLatex();
+  lbl->SetTextFont(43);
+  lbl->SetTextSize(20);
+  lbl->SetNDC();
 
-  // ---------------------------------------
-    
-  scale_down->SetStats(false);
-  scale_down->SetLineWidth(2);
-  SetColor(scale_down,0);
-  scale_down->Draw();
-    
-  scale_up->SetStats(false);
-  scale_up->SetLineWidth(2);
-  SetColor(scale_up,1);
-  scale_up->Draw("same");
-  canv.SaveAs(ofname.c_str());
+  draw({nom});
+  draw({scale_down,scale_up});
+  draw({res_down,res_up});
 
-  // ---------------------------------------
-    
-  res_down->SetStats(false);
-  res_down->SetLineWidth(2);
-  SetColor(res_down,0);
-  res_down->Draw();
-    
-  res_up->SetStats(false);
-  res_up->SetLineWidth(2);
-  SetColor(res_up,1);
-  res_up->Draw("same");
-  canv.SaveAs(ofname.c_str());
-
-  // ---------------------------------------
-
-  canv.SaveAs((ofname+']').c_str());
+  canv->SaveAs((ofname+']').c_str());
+  delete canv;
+  delete lbl;
 
   return 0;
 }
---
+++ src/senum.hh
@@ -0,0 +1,60 @@
+#ifndef senum_hh
+#define senum_hh
+
+#ifndef senum_pp_test
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#endif
+
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/stringize.hpp>
+#include <boost/preprocessor/seq/enum.hpp>
+#include <boost/preprocessor/seq/for_each.hpp>
+
+#include <boost/preprocessor/control/if.hpp>
+#include <boost/preprocessor/comparison/greater.hpp>
+
+#define SENUM_EN_CASE(r, data, elem) \
+  case elem: return BOOST_PP_STRINGIZE(elem);
+
+#define SENUM_STR_CASE(r, enum_name, elem) \
+  if (!str.compare( BOOST_PP_STRINGIZE(elem) )) return elem; else
+
+#define SENUM_CS(r, enum_name, elem) BOOST_PP_IF(BOOST_PP_GREATER(r,2),", ",) BOOST_PP_STRINGIZE(elem)
+
+#define senum(enum_name, values) \
+  struct enum_name { \
+    enum type { BOOST_PP_SEQ_ENUM(values) }; \
+    enum { _nelem = BOOST_PP_SEQ_SIZE(values) }; \
+    static type _enum(const std::string& str) { \
+      BOOST_PP_SEQ_FOR_EACH( SENUM_STR_CASE, nil, values ) \
+      throw std::runtime_error( \
+        "invalid value \""+str+"\" specified for stringized enum " \
+        BOOST_PP_STRINGIZE(enum_name) \
+      ); \
+    } \
+    static const char* _str(type en) { \
+      switch (en) { \
+        BOOST_PP_SEQ_FOR_EACH( SENUM_EN_CASE, nil, values ) \
+      } \
+      return 0; \
+    } \
+    static const char* _str_all() { \
+      return BOOST_PP_SEQ_FOR_EACH( SENUM_CS, nil, values ); \
+    } \
+  }; \
+\
+std::istream& operator>> (std::istream& is, enum_name::type& x) { \
+  std::string str; \
+  is >> str; \
+  x = enum_name::_enum(str); \
+  return is; \
+} \
+\
+std::ostream& operator<< (std::ostream& os, const enum_name::type& x) { \
+  os << enum_name::_str(x); \
+  return os; \
+}
+
+#endif
